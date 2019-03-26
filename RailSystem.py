59ba1f81170f0760574e43b6542fb893ae8d1edd
SeyedMahmoudian <amin.s.mahmoudian@gmail.com>
2019-03-26 18:35:10 -0400

    updates

--- RailSystem.py
+++ RailSystem.py
@@ -1,3 +1,5 @@
+import sys
+
 import Station
 import Trip
 
@@ -24,3 +26,20 @@ class RailSystem:
     def distance_for_trip(self, stopnames=[]):
         trip=self.trip_for_stopnames(stopnames)
         return trip.distance()
+
+    def find_all_trips(self, tripNames, max_dpt=0, max_stops=sys.maxsize, max_distance=sys.maxsize):
+        if max_dpt>0:
+            yield tripNames
+            trip=self.trip_for_stopnames(tripNames)
+            for x in trip.candiate_stop_names():
+                for z in self.find_all_trips(tripNames + [x], max_dpt - 1):
+                    if len(z) <= max_stops:
+                        newTrip=self.trip_for_stopnames(z)
+                        if newTrip.distance <= max_distance:
+                            yield z
+
+
+    def find_trip_from(self,name,max_dpt=10,max_stop=30,max_distance=50):
+        all_trip_names=self.find_all_trips([name],max_dpt,max_stop,max_distance)
+        return all_trip_names
+
